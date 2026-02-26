670976904268bfbf925125ecebecc40e9d5a5fab
adriaclavero2 <adriaclavero2@gmail.com>
2026-02-26 12:46:20 +0100

    Add constructors to Note model

--- src/main/java/model/Note.java
+++ src/main/java/model/Note.java
@@ -10,4 +10,50 @@ public class Note {
     private String content;
     private LocalDateTime createdAt;
 
+    public Note(){}
+
+    public Note(String title, String content){
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Note(ObjectId id, String title, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
