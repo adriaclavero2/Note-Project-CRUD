513392f69f2015ef5b75e8ce2525b18e47983e0b
adriaclavero2 <adriaclavero2@gmail.com>
2026-02-27 10:30:29 +0100

    feat: setup NoteRepository with PojoCodecRegistry

---
+++ src/main/java/persistence/NoteRepository.java
@@ -0,0 +1,23 @@
+package persistence;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import config.MongoDBConnection;
+import model.Note;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+public class NoteRepository {
+    private final MongoCollection<Note> collection;
+
+    public NoteRepository() {
+        CodecRegistry pojoCodecRegistry = fromRegistries(
+                MongoClientSettings.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build())
+        );
+    }
+}
